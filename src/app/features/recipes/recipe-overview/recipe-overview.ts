7625baa1ff86dfb26fd64528c31062d2338264cd
JanTHorstmann <janhorstmann@yahoo.de>
2025-12-09 05:02:52 +0100

    create feature components

---
+++ src/app/features/recipes/recipe-overview/recipe-overview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-overview',
+  imports: [],
+  templateUrl: './recipe-overview.html',
+  styleUrl: './recipe-overview.scss',
+})
+export class RecipeOverview {
+
+}
---
+++ src/app/features/shopping_list/list-edit/list-edit.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-edit',
+  imports: [],
+  templateUrl: './list-edit.html',
+  styleUrl: './list-edit.scss',
+})
+export class ListEdit {
+
+}
