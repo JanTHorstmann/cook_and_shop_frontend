6829bcbb8dd3ed077ce782f10aa59f9a269f407f
JanTHorstmann <janhorstmann@yahoo.de>
2025-12-26 12:56:19 +0100

    navbar and recipe overview on daschboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
-    { path: "", component: Login },
+    { path: "login/", component: Login },
+    { path: "", component: Dashboard },
 ];
--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../shared/components/navbar/navbar';
+import { RecipeOverview } from '../recipes/recipe-overview/recipe-overview';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    Navbar,
+    RecipeOverview,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+
+  ngOnInit() {
+    console.log('start');
+    
+  }
 
 }
